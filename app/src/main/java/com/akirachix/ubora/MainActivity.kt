180f1d5016fdb5d2f013c41e8b025980b13e5197
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-07-13 18:15:46 +0300

    added pages to ubora cbc mobile aoolication

--- app/src/main/java/com/akirachix/ubora/MainActivity.kt
+++ app/src/main/java/com/akirachix/ubora/MainActivity.kt
@@ -1,20 +1,78 @@
 package com.akirachix.ubora
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.ubora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView2.setOnClickListener {
+            val intent = Intent(this,HomepgeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            validateRegistration()
+        }
+        }
+        fun validateRegistration() {
+            clearErrors()
+            var formError = false
+            val firstName = binding.etFirstName.text.toString()
+            if (firstName.isBlank()) {
+                binding.tilFirstName.error ="First Name is required"
+            }
+            val lastName = binding.etLastName.text.toString()
+            if (lastName.isBlank()) {
+                formError = true
+                binding.tilLastName.error = "Last Name is required"
+            }
+            val email = binding.etEmail.text.toString()
+            if (email.isBlank()) {
+                formError = true
+                binding.etEmail.error = "Email is required"
+            }
+            val Password = binding.etPassword.text.toString()
+            if (Password.isBlank()) {
+                formError = true
+                binding.tilPassword.error = "Password is required"
+            }
+
+            val ConfirmPassword = binding.etConfirmPassword.text.toString()
+            if (ConfirmPassword.isBlank()) {
+                formError = true
+                binding.tilConfirmPassword.error = "Confirm password to proceed"
+            }
+            val SchoolName = binding.etSchoolName.text.toString()
+            if (ConfirmPassword.isBlank()) {
+                formError = true
+                binding.tilShoolName.error = "School name is required"
+            }
+
+            if (Password != ConfirmPassword) {
+                formError = true
+                binding.tilConfirmPassword.error = "Password mismatch"
+            }
+            if (formError != true) {
+            }
+        }
+
+        fun clearErrors() {
+            binding.tilFirstName.error = null
+            binding.tilLastName.error = null
+            binding.tilEmail.error = null
+            binding.tilShoolName.error = null
+            binding.tilPassword.error = null
+            binding.tilConfirmPassword.error = null
+
+
         }
     }
-}
