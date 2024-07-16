53d327923f04460ec526ab04bc7f214c461c50d9
Latifa-Umunyana <latifahumunyana@gmail.com>
2024-07-16 07:45:02 +0300

    adding files

---
+++ app/src/main/java/com/akirachix/uboracbc/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.akirachix.uboracbc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akirachix.uboracbc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFinish.setOnClickListener{
+            val intent = Intent( this, PracticalPage::class.java)
+
+            startActivity(intent)
+        }
+    }
+}
